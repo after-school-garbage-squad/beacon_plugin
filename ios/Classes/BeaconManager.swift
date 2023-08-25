b89465c44cdeee4dc36e9782755c83de408664d3
shiron <shiron4710.dev@gmail.com>
2023-08-25 16:41:11 +0900

    style: format

--- ios/Classes/BeaconManager.swift
+++ ios/Classes/BeaconManager.swift
@@ -1,9 +1,8 @@
-import CoreLocation
 import CoreBluetooth
+import CoreLocation
 import Flutter
 
-class BeaconManager: NSObject, ObservableObject, CBCentralManagerDelegate
-{
+class BeaconManager: NSObject, ObservableObject, CBCentralManagerDelegate {
   var beaconServiceUUIDs: [CBUUID] = []
   var wantScan = false
 
@@ -18,46 +17,49 @@ class BeaconManager: NSObject, ObservableObject, CBCentralManagerDelegate
     self.beaconServiceUUIDs = beaconServiceUUIDs.map { CBUUID(string: $0) }
   }
 
-  func startScan(){
+  func startScan() {
     wantScan = true
-    if(centralManager.state == CBManagerState.poweredOn){
+    if centralManager.state == CBManagerState.poweredOn {
       centralManager.scanForPeripherals(withServices: beaconServiceUUIDs, options: nil)
     }
   }
 
-  func stopScan(){
+  func stopScan() {
     centralManager.stopScan()
   }
 
   func centralManagerDidUpdateState(_ central: CBCentralManager) {
-    if(central.state == CBManagerState.poweredOn){
-      if(wantScan){
+    if central.state == CBManagerState.poweredOn {
+      if wantScan {
         startScan()
       }
     }
   }
 
-  func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi rssi: NSNumber) {
+  func centralManager(
+    _ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
+    advertisementData: [String: Any], rssi rssi: NSNumber
+  ) {
     // TODO: Refactor
 
-        var beaconDataList: [BeaconData] = []
-        if let serviceData = advertisementData[CBAdvertisementDataServiceDataKey] as? [CBUUID: Data] {
-            for (serviceUUID, data) in serviceData {
-              for uuid in beaconServiceUUIDs{
-                if serviceUUID == uuid {
-                    let frameType = data[0]
-                    if frameType == 0x02 {
-                        let hwid = data.subdata(in: 1..<6)
-                        let hwidStr = hwid.map { String(format: "%02X", $0) }.joined()
-                        if let beaconData = BeaconData.fromList([uuid.uuidString, hwidStr, rssi]) {
-                          beaconDataList.append(beaconData)
-                        }
-                    }
-                }
+    var beaconDataList: [BeaconData] = []
+    if let serviceData = advertisementData[CBAdvertisementDataServiceDataKey] as? [CBUUID: Data] {
+      for (serviceUUID, data) in serviceData {
+        for uuid in beaconServiceUUIDs {
+          if serviceUUID == uuid {
+            let frameType = data[0]
+            if frameType == 0x02 {
+              let hwid = data.subdata(in: 1..<6)
+              let hwidStr = hwid.map { String(format: "%02X", $0) }.joined()
+              if let beaconData = BeaconData.fromList([uuid.uuidString, hwidStr, rssi]) {
+                beaconDataList.append(beaconData)
               }
             }
+          }
         }
-
-        BeaconPlugin.flutterBeaconApi?.onScanned(beaconDataList: beaconDataList){}
+      }
     }
+
+    BeaconPlugin.flutterBeaconApi?.onScanned(beaconDataList: beaconDataList) {}
+  }
 }
--- lib/permission_manager.dart
+++ lib/permission_manager.dart
@@ -10,28 +10,27 @@ class BeaconPermissionManager {
 
   List<Permission> get permissions => _permissions;
 
-  List<PermissionWithService> get permissionsWithService => _permissions.whereType<PermissionWithService>().toList();
+  List<PermissionWithService> get permissionsWithService =>
+      _permissions.whereType<PermissionWithService>().toList();
 
   Future<Map<Permission, PermissionStatus>> getPermissionStatuses() async {
-    final Map<Permission, PermissionStatus> statuses =
-    Map.fromIterables(
-        _permissions,
-        await Future.wait(_permissions.map((e) => e.status))
-    );
+    final Map<Permission, PermissionStatus> statuses = Map.fromIterables(
+        _permissions, await Future.wait(_permissions.map((e) => e.status)));
     return statuses;
   }
 
   Future<Map<PermissionWithService, ServiceStatus>> getServiceStatuses() async {
     final Map<PermissionWithService, ServiceStatus> statuses =
-    Map.fromIterables(
-        permissionsWithService,
-        await Future.wait(permissionsWithService.map((e) => e.serviceStatus))
-    );
+        Map.fromIterables(
+            permissionsWithService,
+            await Future.wait(
+                permissionsWithService.map((e) => e.serviceStatus)));
     return statuses;
   }
 
   Future<Map<Permission, PermissionStatus>> requestPermissions() async {
-    final Map<Permission, PermissionStatus> statuses = await _permissions.request();
+    final Map<Permission, PermissionStatus> statuses =
+        await _permissions.request();
     return statuses;
   }
 }
