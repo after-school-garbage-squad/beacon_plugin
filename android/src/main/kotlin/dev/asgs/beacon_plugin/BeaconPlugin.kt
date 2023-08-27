50ddaceeaafc79223b6d07a4ebdd0ef2b2a773e2
shiron <shiron4710.dev@gmail.com>
2023-08-27 23:11:22 +0900

    feat(android): di context

--- android/src/main/kotlin/dev/asgs/beacon_plugin/BeaconPlugin.kt
+++ android/src/main/kotlin/dev/asgs/beacon_plugin/BeaconPlugin.kt
@@ -1,13 +1,62 @@
 package dev.asgs.beacon_plugin
 
+import BeaconManagerApi
+import android.app.Activity
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 
-class BeaconPlugin: FlutterPlugin {
+@RequiresApi(Build.VERSION_CODES.O)
+class BeaconPlugin: FlutterPlugin,
+  MethodCallHandler,
+  ActivityAware {
+
+  private lateinit var activity: Activity
+  private lateinit var context: Context
+
+  private val beaconManagerApi: BeaconManagerApiImpl by lazy { BeaconManagerApiImpl(context) }
+
   override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    // unnecessary
+    Log.d("BeaconPlugin", "onAttachedToEngine")
+    context = binding.applicationContext
+
+    BeaconManagerApiImpl.setupBackgroundScanJob()
+    BeaconManagerApi.setUp(
+      binaryMessenger = binding.binaryMessenger,
+      api = beaconManagerApi
+    )
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    // unnecessary
+    Log.d("BeaconPlugin", "onDetachedFromEngine")
   }
+
+  override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+    Log.d("BeaconPlugin", "onMethodCall")
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    Log.d("BeaconPlugin", "onAttachedToActivity")
+    activity = binding.activity
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    Log.d("BeaconPlugin", "onDetachedFromActivityForConfigChanges")
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    Log.d("BeaconPlugin", "onReattachedToActivityForConfigChanges")
+  }
+
+  override fun onDetachedFromActivity() {
+    Log.d("BeaconPlugin", "onDetachedFromActivity")
+  }
+
 }
--- example/android/app/src/main/kotlin/dev/asgs/beacon_plugin_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/dev/asgs/beacon_plugin_example/MainActivity.kt
@@ -1,19 +1,13 @@
 package dev.asgs.beacon_plugin_example
 
-import BeaconManagerApi
 import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.multidex.MultiDex
 import dev.asgs.beacon_plugin.BeaconManagerApiImpl
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import org.altbeacon.beacon.BeaconManager
 
 class MainActivity: FlutterActivity() {
-    private var beaconManager: BeaconManager? = null
-
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
         MultiDex.install(this)
@@ -21,16 +15,12 @@ class MainActivity: FlutterActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        BeaconManagerApiImpl.setupBackgroundScanJob()
+
         BeaconManagerApiImpl.setupNotification(
             notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager,
             notificationId = 777,
             notificationChannelId = "beacon_plugin_example",
             notificationIcon = R.drawable.ic_android
-        )
-        BeaconManagerApi.setUp(
-            binaryMessenger = flutterEngine.dartExecutor.binaryMessenger,
-            api = BeaconManagerApiImpl(this.applicationContext)
         )
     }
 }
